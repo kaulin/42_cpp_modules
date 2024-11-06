676b15c54d6fda71dddb725c812caab0d933e7be
Jaakko Junttila <jajuntti@student.hive.fi>
2024-11-06 09:22:05 +0200

    Updates template

--- templates/Class.cpp
+++ templates/Class.cpp
@@ -14,10 +14,10 @@
 
 // Construct
 Class::Class () {}
-Class::Class (std::string name) { _name = name; }
+Class::Class ( std::string name ) { _name = name; }
 // Destruct
 Class::~Class() { std::cout << _name << std::endl; }
 // Get
-std::string Class::getName() { return _name; }
+std::string	Class::getName() { return _name; }
 // Set
-void Class::setName(std::string name) { _name = name; }
+void	Class::setName( std::string name ) { _name = name; }
