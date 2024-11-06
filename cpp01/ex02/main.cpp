e3969d8cd2114fb1820c4b53b0faf1217ae6d211
Jaakko Junttila <jajuntti@student.hive.fi>
2024-11-06 09:07:53 +0200

    Makes ex02 work by changing main.c to main.cpp :)

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:50:30 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/11/05 15:18:27 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/11/06 08:45:05 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ int	main(void) {
 	std::string	&stringREF = stringVAR;
 	
 	std::cout << "Address of stringVAR: " << &stringVAR << std::endl;
-	std::cout << "Address held by stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
 	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
 
 	std::cout << "Value of stringVAR: " << stringVAR << std::endl;
 	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
-	std::cout << "Value of stringREF: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
 
 	return 0;
 }
