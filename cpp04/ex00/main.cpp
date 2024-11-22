db76968efa7f792ebe8810aaa77160bfa1d8153f
Jaakko Junttila <jajuntti@student.hive.fi>
2024-11-22 15:21:14 +0200

    Removes redundant comments

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -9,31 +9,31 @@
 int main()
 {
 	std::cout << "Testing working animals:" << std::endl;
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
+	const Animal* animalAnimal = new Animal();
+	const Animal* animalDog = new Dog();
+	const Animal* animalCat = new Cat();
+	std::cout << animalDog->getType() << " " << std::endl;
+	std::cout << animalCat->getType() << " " << std::endl;
+	animalCat->makeSound();
+	animalDog->makeSound();
+	animalAnimal->makeSound();
 	
 	std::cout << "\nTesting wrong animals:" << std::endl;
-	const WrongAnimal* wrongMeta = new WrongAnimal();
-	const WrongAnimal* k = new WrongCat();
+	const WrongAnimal* wrongAnimalAnimal = new WrongAnimal();
+	const WrongAnimal* wrongAnimalCat = new WrongCat();
 	const WrongCat* wrongCat = new WrongCat();
-	std::cout << k->getType() << " " << std::endl;
+	std::cout << wrongAnimalCat->getType() << " " << std::endl;
 	std::cout << wrongCat->getType() << " " << std::endl;
-	k->makeSound(); //will output the cat sound!
+	wrongAnimalCat->makeSound();
 	wrongCat->makeSound();
-	wrongMeta->makeSound();
+	wrongAnimalAnimal->makeSound();
 
 	std::cout << "\nDestroying:" << std::endl;
-	delete meta;
-	delete j;
-	delete i;
-	delete wrongMeta;
+	delete animalAnimal;
+	delete animalDog;
+	delete animalCat;
+	delete wrongAnimalAnimal;
 	delete wrongCat;
-	delete k;
+	delete wrongAnimalCat;
 	return 0;
 }
