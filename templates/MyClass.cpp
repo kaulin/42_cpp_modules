19afd1d25fd6939fd4cab0b2995fadb6a1a845f7
Jaakko Junttila <jajuntti@student.hive.fi>
2024-11-26 13:48:08 +0200

    Switches from endl to \n

--- templates/MyClass.cpp
+++ templates/MyClass.cpp
@@ -2,23 +2,23 @@
 
 // Default Constructor
 MyClass::MyClass () {
-	std::cout << "Default constructor called." << std::endl;
+	std::cout << "Default constructor called.\n";
 }
 // Parameterized Constructor
 MyClass::MyClass (const std::string& name) : _name(name) {
-	std::cout << "Parameterized constructor called." << std::endl;
+	std::cout << "Parameterized constructor called.\n"
 }
 // Copy Constructor
 MyClass::MyClass(const MyClass& other) : _name(other._name) {
-	std::cout << "Copy constructor called." << std::endl;
+	std::cout << "Copy constructor called.\n";
 }
 // Destructor
 MyClass::~MyClass() {
-	std::cout << "Destructor called." << std::endl;
+	std::cout << "Destructor called.\n";
 }
 // Copy Assignment Operator
 MyClass& MyClass::operator=(const MyClass& other) {
-	std::cout << "Copy assignment operator called." << std::endl;
+	std::cout << "Copy assignment operator called.\n";
 	if (this == &other) return *this;
 	_name = other._name;
 	return *this;
