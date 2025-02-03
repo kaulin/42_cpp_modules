2a1c3c2e113597c6e1ac2d326a2ed1ab79fe090c
Jaakko Junttila <junttila@proton.me>
2025-02-03 17:59:50 +0200

    Adds const type handling to ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,3 +6,8 @@ template <typename T> void iter(T* arr, int len, void (*f)(T)) {
 	for (int i = 0; i < len; i++)
 		f(arr[i]);
 }
+
+template <typename T> void iter(T* arr, int len, void (*f)(const T&)) {
+	for (int i = 0; i < len; i++)
+		f(arr[i]);
+}
