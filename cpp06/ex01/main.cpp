297ef9dde25e452e9a9d40df264ca2cf2fb6cea3
Jaakko Junttila <junttila@proton.me>
2025-02-03 12:06:01 +0200

    Updates ex01 main printouts

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -11,12 +11,12 @@ int main (int argc, char** argv) {
 
 	data.name = argv[1];
 
-	std::cout <<  "TESTING pointer serialization!\n";
-	std::cout << "  Pointer ["<< &data << "] contains data with name: " << data.name << "\n";
+	std::cout <<  "TESTING POINTER SERIALIZATION\n";
+	std::cout << "Pointer ["<< &data << "] contains data with name: " << data.name << "\n";
 	serialized_ptr = Serializer::serialize(&data);
-	std::cout << "  Serialized pointer ["<< serialized_ptr << "]\n";
+	std::cout << "Serialized pointer ["<< serialized_ptr << "]\n";
 	deserialized_ptr = Serializer::deserialize(serialized_ptr);
-	std::cout << "  Deserialized pointer to data packet ["<< deserialized_ptr << "]\n";
-	std::cout << "OUTCOME: " << ((&data == deserialized_ptr) ? "OK - pointers match\n" : "KO - pointers don't match\n");
+	std::cout << "Deserialized pointer to data packet ["<< deserialized_ptr << "]\n";
+	std::cout << "	OUTCOME: " << ((&data == deserialized_ptr) ? "OK - pointers match\n" : "KO - pointers don't match\n");
 	return 0;
 }
