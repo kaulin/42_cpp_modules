6d021f35fdd71f68150d83777b347700b59a0e09
Jaakko Junttila <jajuntti@student.hive.fi>
2025-02-13 17:03:47 +0200

    Adds proper support for multitype functions and updates gitignore

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -2,12 +2,8 @@
 
 #include <iostream>
 
-template <typename T> void iter(T* arr, size_t len, void (*f)(T&)) {
-	for (size_t i = 0; i < len; i++)
-		f(arr[i]);
-}
-
-template <typename T> void iter(const T* arr, size_t len, void (*f)(const T&)) {
+template <typename T, typename F>
+void iter(T* arr, size_t len, F f) {
 	for (size_t i = 0; i < len; i++)
 		f(arr[i]);
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -4,18 +4,18 @@
 int main(int argc, char** argv) {
 	if (argc > 1) {
 		std::cout << "Testing template on arg array: \n";
-		::iter(argv, argc, printItem);
+		iter(argv, argc, printItem<char*>);
 	}
 	else {
 		std::string strArray[] = {"All", "your", "base", "are", "belong", "to", "us", "!"};
 		int	intArray[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 		const int intArrayConst[] = {0, 42, 666};
 		std::cout << "Testing template on string array: \n";
-		::iter(strArray, 8, printItem);
+		iter(strArray, 8, printItem<std::string>);
 		std::cout << "Testing template on int array: \n";
-		::iter(intArray, 10, printItem);
+		iter(intArray, 10, printItem<int>);
 		std::cout << "Testing template on const int array: \n";
-		::iter(intArrayConst, 3, printItem);
+		iter(intArrayConst, 3, printItem<const int>);
 	}
 	return 0;
 }
