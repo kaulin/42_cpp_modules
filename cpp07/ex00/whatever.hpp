611da5cdce33bc0d4d8571dcb0b44723e3f12bb7
Jaakko Junttila <jajuntti@student.hive.fi>
2025-02-13 12:48:30 +0200

    Defaults to second argument in equal casaes, as per the subject

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -9,9 +9,9 @@ template <typename T> void swap(T& a, T& b) {
 }
 
 template <typename T> const T min(const T& a, const T& b) {
-	return (a <= b ? a : b);
+	return (a < b ? a : b);
 }
 
 template <typename T> const T max(const T& a, const T& b) {
-	return (a >= b ? a : b);
+	return (a > b ? a : b);
 }
