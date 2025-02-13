869ac5e1e220811edf7f0bd26d611c02215718c4
Jaakko Junttila <jajuntti@student.hive.fi>
2025-02-13 16:38:53 +0200

    Adds support for cases of passing a const function

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -7,6 +7,11 @@ template <typename T> void iter(T* arr, size_t len, void (*f)(T&)) {
 		f(arr[i]);
 }
 
+template <typename T> void iter(const T* arr, size_t len, void (*f)(const T&)) {
+	for (size_t i = 0; i < len; i++)
+		f(arr[i]);
+}
+
 template <typename T> void printItem( T item) {
 	std::cout << item << "\n";
 }
