bb505418706e979d15ff04ecb3c05e4ee7f43532
Jaakko Junttila <jajuntti@student.hive.fi>
2025-02-18 14:32:56 +0200

    Comments out assertion to check for correct container value type

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-# include <algorithm>
-#include <type_traits>
+#include <algorithm>
+//#include <type_traits>
 
 template <typename T>
 typename T::iterator easyfind(T& source, int target) {
-	static_assert(std::is_integral<typename T::value_type>::value, "easyfind requires a container of integers");
+	// static_assert(std::is_integral<typename T::value_type>::value, "easyfind requires a container of integers");
 	return std::find(source.begin(), source.end(), target);
 };
