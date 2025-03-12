54b361fba284aa7932423c30db07dcf080bead08
Jaakko Junttila <jajuntti@student.hive.fi>
2025-03-12 11:22:10 +0200

    Hides purposelessly exposed member functions

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -15,18 +15,14 @@
 class BitcoinExchange {
 private:
 	std::map<time_t, float> _data;
-public:
-	BitcoinExchange();
 	BitcoinExchange(const BitcoinExchange& other);
-	~BitcoinExchange();
 	BitcoinExchange& operator=(const BitcoinExchange& other);
-
 	std::string parseLine(const std::string& line, char delim, time_t& date, float& value) const;
 	time_t parseDate(const std::string& dateString) const;
 	float getTotalValue(time_t date, float count) const;
-
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
 	void initDatabase(const std::string& databaseFilePath);
 	void calculateTotals(const std::string& inputFilePath) const;
-	
-	class BitcoinExchangeException : public std::exception {};
 };
